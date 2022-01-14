c1f093bb059829ef5e79c7712b99f5bd235d0630
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-14 18:28:56 -0300

    Sidebar terminado

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './Sidebar.css';
+import Logo from '../../images/logo.png'
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <section className="">
-      <h1>Sidebar</h1>
-    </section>
+    <aside className="sidebarContainer">
+    <div className='iconTitleSideBar'>
+        <Link className='linkHeaderSidebar' to="#">
+            <img src={Logo} alt="ej" />
+            <span>Economysa</span>
+        </Link>
+      <button><i className="fas fa-lock"></i></button>
+    </div>
+    <div className='linksContainer'>
+        <Link className='linkSidebar' to="#">Dasboard <i className="fas fa-home"></i></Link>
+        <Link className='linkSidebar' to="#">
+            Seguridad 
+            <div className='iconContainerSeguridad'>
+            <i className="fas fa-chevron-down"></i>
+            <i className="fas fa-lock lockLink"></i>
+            </div>
+        </Link>
+        <Link className='linkSidebar' to="#">
+            Motor
+            <div>
+                <i className="fas fa-chevron-down"></i>
+                <i className="fas fa-globe-americas"></i>
+            </div>
+        </Link>
+    </div>
+    </aside>
   );
 }
 

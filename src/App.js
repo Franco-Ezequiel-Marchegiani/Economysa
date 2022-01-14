6de32a2f56e445e2075e75360fd9a88bed218adb
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-14 20:52:51 -0300

    Home terminado

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Logo from './images/logo.png'
 
 function App() {
   return (
     <section className="homeContainer">
-      <section>
-        <Sidebar/>
-      </section>
-      <section>
+      <Sidebar/>
+      <section className='fondoHome'>
       <Header/>
-      <h1>home</h1>
+      <div className='divEspacioHome'><p>...</p></div>
+      <div className='infoPrincipalHome'>
+      <div className='infoPrincipalContainer'>
+        <h1>Empty Page</h1>
+        <p>Use this page to start from scratch and place your custom content.</p>
+        <button>Home</button>
+      </div>
+      </div>
+      <div className='homeRellenoContainer'>
+          <img src={Logo} alt="ej" />
+          <div className='homeTextoRellenoContainer'>
+              <p>Jeferson Cieza</p>
+              <span>Development</span>
+          </div>
+      </div>
       </section>
 
     </section>

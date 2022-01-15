d53d1356fe2d72e3f9c3ad7c08770ef16c47b559
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-15 17:00:01 -0300

    Creacion archivos de formularios

---
+++ src/components/Formularios/FormularioMotores.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Formularios.css';
+
+function FormularioMotores() {
+  return (
+    <section className="">
+      <h1>FormularioMotores</h1>
+    </section>
+  );
+}
+
+export default FormularioMotores;
---
+++ src/components/Formularios/FormularioUsuarios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Formularios.css';
+
+function FormularioUsuarios() {
+  return (
+    <section className="">
+      <h1>FormularioUsuarios</h1>
+    </section>
+  );
+}
+
+export default FormularioUsuarios;

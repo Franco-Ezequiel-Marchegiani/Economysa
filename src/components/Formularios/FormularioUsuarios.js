fc9deee4b613f8a73de0f30dbe3ee12d95927eca
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-15 17:24:35 -0300

    Creacion array inputs creacion usuarios

--- src/components/Formularios/FormularioUsuarios.js
+++ src/components/Formularios/FormularioUsuarios.js
@@ -5,6 +5,68 @@ import Sidebar from '../Sidebar/Sidebar';
 import './Formularios.css';
 
 function FormularioUsuarios() {
+    let arrayInputsUsers = [
+        {
+            id:1,
+            title: "Código",
+            inputType: "number"
+        },
+        {
+            id:2,
+            title: "Nombre Cliente",
+            inputType: "text"
+        },
+        {
+            id:3,
+            title: "Código Padre",
+            inputType: "text"
+        },
+        {
+            id:4,
+            title: "Razón Social",
+            inputType: "text"
+        },
+        {
+            id:5,
+            title: "Tipo Documento",
+            inputType: "text"
+        },
+        {
+            id:6,
+            title: "Nro. Documento",
+            inputType: "number"
+        },
+        {
+            id:7,
+            title: "Celular",
+            inputType: "number"
+        },
+        {
+            id:8,
+            title: "Correo",
+            inputType: "email"
+        },
+        {
+            id:9,
+            title: "Limite Cred",
+            inputType: "number"
+        },
+        {
+            id:10,
+            title: "Limite Doc",
+            inputType: "number"
+        },
+        {
+            id:11,
+            title: "Dirección fiscal",
+            inputType: "text"
+        },
+        {
+            id:12,
+            title: "Ubigeo",
+            inputType: "text"
+        },
+    ]
   return (
     <section className="globalContainerContent">
       <Sidebar/>

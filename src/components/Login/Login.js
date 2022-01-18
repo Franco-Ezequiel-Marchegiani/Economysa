51f6330f7cad0c32a0aa3c8ff869fcd60c52ee7a
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-18 19:15:41 -0300

    Solución background imagen, la re contra pelee

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,36 +5,42 @@ import Logo from '../../images/logo.png'
 function Login() {
   return (
     <section className="loginContainerAll">
-      <div>
-        <span className='btn-darkModeLogin'>a</span>
-      </div>
-      <main className='loginContainer'>
         <div>
-          <img src={Logo} alt="" />
-          <h1>Ingresa a Economysa</h1>
-          <p>Bienvenido. Ingrese sus credenciales.</p>
-          <Form>
-              <Form.Group className="mb-3 inputRegister" controlId="formBasicEmail">
-                <Form.Label>Correo electrónico</Form.Label>
-                  <div>
-                    <Form.Control type="email" placeholder="Ingresa tu correo electrónico..." />
-                    <i className="fas fa-lock"></i>
-                  </div>
-              </Form.Group>
+            <main className='prueba'>
+                <div className='loginContainer'>
+                    <div>
+                      <span className='btn-darkModeLogin'>a</span>
+                    </div>
+                    <div className='loginContent'>
+                    <div className='iconLoginContainer'>
+                      <img src={Logo} alt="" />
+                    </div>
+                      <h1 className='titleLogin'><span>Ingresa a</span> <span>Economysa</span></h1>
+                      <p className='parrafoLogin'>Bienvenido. Ingrese sus credenciales.</p>
+                      <Form className='formLogin'>
+                          <Form.Group className="mb-3 inputRegister" controlId="formBasicEmail">
+                            <Form.Label>Correo electrónico</Form.Label>
+                              <div>
+                                <Form.Control type="email" placeholder="Ingresa tu correo electrónico..." />
+                                <i className="fas fa-lock"></i>
+                              </div>
+                          </Form.Group>
 
-              <Form.Group className="mb-3 inputRegister" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                  <div>
-                    <Form.Control type="password" placeholder="Ingresa tu contraseña..." />
-                    <i className="fas fa-lock"></i>
+                          <Form.Group className="mb-3 inputRegister" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                              <div>
+                                <Form.Control type="password" placeholder="Ingresa tu contraseña..." />
+                                <i className="fas fa-lock"></i>
+                              </div>
+                          </Form.Group>
+                          <button className='btn-datosFormRegister' type="submit">
+                            Ingresar
+                          </button>
+                      </Form>
+                    </div>
                   </div>
-              </Form.Group>
-              <button className='btn-datosFormRegister' type="submit">
-                Ingresar
-              </button>
-          </Form>
-        </div>
-      </main>
+            </main>
+          </div>
     </section>
   );
 }

28b9af848af8605bc5c679a76e30c3bf10c20c52
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-19 19:45:29 -0300

    Guardo ya que conseguí extraer los colores de fondo

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 import ImageSun from '../../images/sun404.svg'
 function NotFound() {
   return (
-            <section className="loginContainerAll">
-                <img className='imageSunNotFound' src={ImageSun} alt="Sun 404" />
-                    <div>
-                        <main>
-                            <div className='loginContainer'>
-                                <div className='loginContent'>
-                                    <span className='span404'>404</span>
-                                    <h1 className='titleNotFound'>Page Not Found</h1>
-                                    <hr className='lineNotFound'/>
-                                    <p className='parraNotFound'>Requested resource is not available right now. Please try again later.</p>
-                                    <Link to="/">
-                                        <button className='btn-datosFormNotFound' type="submit">Go To DashBoard</button>
-                                    </Link>
-                                </div>
-                                </div>
-                        </main>
-                    </div>
-            </section>
+        <section className="notFoundContainerAll">
+            <img className='imageSunNotFound' src={ImageSun} alt="Sun 404" />
+                <div>
+                    <main>
+                        <div className='notFoundContainer'>
+                            <div className='notFoundContent'>
+                                <span className='span404'>404</span>
+                                <h1 className='titleNotFound'>Page Not Found</h1>
+                                <hr className='lineNotFound'/>
+                                <p className='parraNotFound'>Requested resource is not available right now. Please try again later.</p>
+                                <Link to="/">
+                                    <button className='btn-datosFormNotFound' type="submit">Go To DashBoard</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+         </section>
   );
 }
 

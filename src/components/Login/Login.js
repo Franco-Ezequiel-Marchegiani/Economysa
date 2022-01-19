988ba893718b3b7715196b96fd5d753ba1e5a17f
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-19 16:56:26 -0300

    Separé en componentes separados cada login, cosa de no hacer muy largo el componente de Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,53 +1,14 @@
 import React, {useState} from 'react';
 import './Login.css';
-import { Form } from 'react-bootstrap';
-import Logo from '../../images/logo.png'
 import LoginDarkMode from './LoginDarkMode';
+import LoginLightMode from './LoginLightMode';
 function Login() {
   const [darkMode, setDarkMode] = useState(false);
   console.log(darkMode);
   return (
     <section>
-    {darkMode === false ?  
-      <section className="loginContainerAll">
-        <i onClick={() => setDarkMode(true)} class="fas fa-circle btn-darkModeLogin"></i>
-          <div>
-              <main className='prueba'>
-                  <div className='loginContainer'>
-                      <div className='loginContent'>
-                      <div className='iconLoginContainer'>
-                        <img src={Logo} alt="" />
-                      </div>
-                        <h1 className='titleLogin'><span>Ingresa a</span> <span>Economysa</span></h1>
-                        <p className='parrafoLogin'>Bienvenido. Ingrese sus credenciales.</p>
-                        <Form className='formLogin'>
-                            <Form.Group className="mb-3 inputRegister" controlId="formBasicEmail">
-                              <Form.Label>Correo electrónico</Form.Label>
-                                  <Form.Control type="email" placeholder="Ingresa tu correo electrónico..." />
-                                  <i class="fas fa-user"></i>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 inputRegister" controlId="formBasicPassword">
-                              <Form.Label>Contraseña</Form.Label>
-                                  <Form.Control type="password" placeholder="Ingresa tu contraseña..." />
-                                  <i className="fas fa-lock"></i>
-                            </Form.Group>
-                            <button className='btn-datosFormRegister' type="submit">
-                              Ingresar
-                            </button>
-                        </Form>
-                      </div>
-                    </div>
-                    <div className='imagenFondo'>
-
-                    </div>
-              </main>
-            </div>
-      </section>
-
-      : <LoginDarkMode/> }
+    {darkMode === false ? <LoginLightMode/> : <LoginDarkMode/> }
     </section>
-    
   );
 }
 

e9a182e16014d1a792eccf7efd5d9a0971ec98b5
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-22 20:59:21 -0300

    Implementación useEffect en login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Login.css';
 import LoginDarkMode from './LoginDarkMode';
 import LoginLightMode from './LoginLightMode';
 function Login() {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+  }, []);
+
   console.log(darkMode);
   return (
     <section>

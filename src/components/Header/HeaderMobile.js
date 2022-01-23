cc6715a5b3ef7902362d1ce556871cef32898f32
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-23 19:10:56 -0300

    Implementé íconos del header para celulares

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Header.css';
-import Logo from '../../images/fotoEjemplo.jpg'
+import Logo from '../../images/logo.png'
 
 function HeaderMobile() {
   return (
     <header className="headerMobile">
-      <div className='headerContent'>
-          <img src={Logo} alt="ej" />
-          <div className='headerTextContainer'>
-              <p>Jeferson Cieza</p>
-              <span>Development</span>
-          </div>
+      <div className='headerContentMobile'>
+            <div className='logoTitleContainerHeader'>
+                <img src={Logo} alt="ej" />
+                <p>Mirage</p>
+            </div>
+            <div className='headerBtnContainerMobile'>
+                <i className="fas fa-bars"></i>
+                <i className="fas fa-align-right"></i>
+                <i className="fas fa-ellipsis-v"></i>
+            </div>
       </div>
     </header>
   );

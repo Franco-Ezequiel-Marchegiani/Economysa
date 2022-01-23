b0f90a357bfc95b6c943d41fbffad7d30575c3e3
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-22 21:16:43 -0300

    implementación url

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,7 @@ import LoginLightMode from './LoginLightMode';
 function Login() {
   const [darkMode, setDarkMode] = useState(false);
 
+  const url = "{{host}}/oauth/token"
   const user = "angular_client"; 
   const pass = 123456;
   const encodeData = btoa(user + ":" + pass);

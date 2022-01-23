7d0809056eeb6babdec8a5ae4d7ccd2775fe7d9f
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-22 21:06:19 -0300

    Terminé variable requestAPI, queda crear la variable de la base64

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,12 @@ import LoginLightMode from './LoginLightMode';
 function Login() {
   const [darkMode, setDarkMode] = useState(false);
 
+  let borrador = "a"
+  const requestAPI = {
+    method: "POST",
+    "Authorization": "Basic " + borrador,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
   useEffect(() => {
   }, []);
 
